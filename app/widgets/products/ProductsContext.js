7f149256c35845e72f43e7b8254a3c60053113b7
AntoineFachez <anthony.zornig@gmx.de>
2024-10-08 09:02:26 +0200

    introduce products widget

---
+++ app/widgets/products/ProductsContext.js
@@ -0,0 +1,67 @@
+'use client';
+import React, { useCallback, useEffect, useRef } from 'react';
+import { createContext, useContext, useState } from 'react';
+import AppContext from '../../../context/AppContext';
+import { products } from './mockProducts';
+
+const ProductsContext = createContext();
+
+export const ProductsProvider = ({ children }) => {
+  const [selectedWidgetContext, setSelectedWidgetContext] = useState(null);
+  const [displayProducts, setDisplayProducts] = useState(products);
+  const [selectedProducts, setSelectedProducts] = useState(displayProducts);
+  const [productInFocus, setProductInFocus] = useState(false);
+
+  const [searchTerm, setSearchTerm] = useState('');
+  const [isFiltered, setIsFiltered] = useState(false);
+
+  // const handleSearchTermChange = (e) => {
+  //   e.preventDefault();
+
+  //   setSearchTerm(e.target.value);
+  //   setActiveSearchTerm(e.target.value);
+  // };
+  const handleResetFiltered = () => {
+    setSelectedProducts(displayUserStories);
+    setIsFiltered(false);
+  };
+  useEffect(() => {
+    setSelectedProducts(
+      displayProducts.filter((product) =>
+        product.product_name.toLowerCase().includes(searchTerm.toLowerCase())
+      )
+    );
+
+    return () => {};
+  }, [searchTerm]);
+  useEffect(() => {
+    setSelectedProductProducts(displayProducts);
+
+    return () => {};
+  }, [displayProducts]);
+  return (
+    <ProductsContext.Provider
+      value={{
+        // selectedWidgetContext,
+        // setSelectedWidgetContext,
+        displayProducts,
+        setDisplayProducts,
+        selectedProducts,
+        setSelectedProducts,
+        productInFocus,
+        setProductInFocus,
+        isFiltered,
+        searchTerm,
+        setSearchTerm,
+        // handleSearchTermChange,
+        handleResetFiltered,
+      }}
+    >
+      {children}
+    </ProductsContext.Provider>
+  );
+};
+export default ProductsContext;
+export const ProductsState = () => {
+  return useContext(ProductsContext);
+};
