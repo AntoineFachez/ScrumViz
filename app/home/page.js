f5c0bd49cb9abb10d55499b5226438716e3b8b48
AntoineFachez <anthony.zornig@gmx.de>
2024-10-12 00:52:01 +0200

    refactor home to have own user data

--- app/home/page.js
+++ app/home/page.js
@@ -1,12 +1,27 @@
 'use client';
 import React from 'react';
-// import Profile from '../profile/page';
+import Profile from '../profile/page';
+import { useSession } from 'next-auth/react';
 
 export default function Home() {
+  const { data: session } = useSession();
+
+  if (!session) {
+    return <div>Loading...</div>; // Display a loading message
+  }
+
   return (
     <div>
-      <div>Home</div>
-      {/* <Profile /> */}
+      <div>Profile</div>
+      {session && ( // Conditional rendering
+        <div>
+          <p>Logged in as {session.user.email}</p>
+          <p>Using provider: {session.provider}</p>
+          <img src={session.user.image} alt={session.user.name} />
+          <p>Name: {session.user.name}</p>
+          <p>Email: {session.user.email}</p>
+        </div>
+      )}
     </div>
   );
 }
