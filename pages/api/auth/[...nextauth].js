e78113b10b1cef88eb4b724ec65a89cf7ff9dc5b
AntoineFachez <anthony.zornig@gmx.de>
2024-10-12 00:15:00 +0200

    refactor home with userData

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,9 +1,8 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
+import GithubProvider from 'next-auth/providers/github';
 import { FirebaseAdapter } from '@next-auth/firebase-adapter';
 import { auth } from '../../../firebase/firebase';
-
-console.log(FirebaseAdapter, auth);
 
 export const authOptions = {
   providers: [
