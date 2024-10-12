3ad032d0336920e30ca40cceabd2f5315270a3c3
AntoineFachez <anthony.zornig@gmx.de>
2024-10-12 08:42:41 +0200

    refactor authOptions for Providers, callback

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,7 +1,7 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 import GithubProvider from 'next-auth/providers/github';
-import { FirebaseAdapter } from '@next-auth/firebase-adapter';
+// import { FirebaseAdapter } from '@next-auth/firebase-adapter';
 import { auth } from '../../../firebase/firebase';
 
 export const authOptions = {
@@ -9,12 +9,32 @@ export const authOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      // authorization: {
+      //   params: {
+      //     redirect_uri: `${process.env.NEXTAUTH_URI}/api/auth/callback/google`,
+      //   },
+      // },
     }),
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
+      // authorization: {
+      //   params: {
+      //     redirect_uri: `${process.env.NEXTAUTH_URI}/api/auth/callback/github`,
+      //   },
+      // },
     }),
   ],
+  // adapter: FirebaseAdapter(auth),
+  callbacks: {
+    async redirect({ url, baseUrl }) {
+      // Allows relative callback URLs
+      if (url.startsWith('/')) return `${baseUrl}${url}`;
+      // Allows callback URLs on the same origin
+      else if (new URL(url).origin === new URL(baseUrl).origin) return url;
+      return baseUrl;
+    },
+  },
 };
 
 export default NextAuth(authOptions);
