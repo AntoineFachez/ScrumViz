d52e68e32501890d47acb90ebf5ac206f6919514
AntoineFachez <anthony.zornig@gmx.de>
2024-10-12 18:46:24 +0200

    implement loadingCircle

--- app/widgets/auth/signUplogInElements/LogInProviders.js
+++ app/widgets/auth/signUplogInElements/LogInProviders.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import { Box, Button } from '@mui/material';
+import { GitHub, Google } from '@mui/icons-material';
 
 export default function LogInProviders({ widgetProps }) {
-  const { handleSubmit, styled } = widgetProps;
+  const { handleSubmit, styled, session } = widgetProps;
+
+  // if (session) {
+  //   return (
+  //     <>
+  //       Signed in as {session?.user?.email} <br />
+  //       <button onClick={() => signOut()}>Sign out</button>
+  //     </>
+  //   );
+  // }
+  // return (
+  //   <>
+  //     Not signed in <br />
+  //     <button onClick={() => signIn()}>Sign in</button>
+  //   </>
+  // );
   return (
     <Box
       component={'ul'}
@@ -12,13 +28,15 @@ export default function LogInProviders({ widgetProps }) {
       }}
     >
       <Button
-        sx={styled.menuButtonText.action}
+        startIcon={<Google sx={{ ...styled.menuButtonText.action }} />}
+        sx={{ ...styled.menuButtonText.action, color: 'white' }}
         onClick={(e) => handleSubmit(e, 'google')}
       >
         Sign in with Google
       </Button>{' '}
       <Button
-        sx={styled.menuButtonText.action}
+        startIcon={<GitHub sx={{ ...styled.menuButtonText.action }} />}
+        sx={{ ...styled.menuButtonText.action, color: 'white' }}
         onClick={(e) => handleSubmit(e, 'github')}
       >
         Sign in with GitHub
--- next.config.mjs
+++ next.config.mjs
@@ -23,6 +23,28 @@ const nextConfig = {
 
     return config;
   },
+
+  // webpack: (config, { isServer }) => {
+  //   // Only add the file-loader rule on the client-side
+  //   if (!isServer) {
+  //     config.module.rules.push({
+  //       test: /\.(woff|woff2|eot|ttf|otf)$/i, // Match font files
+  //       type: 'asset/resource', // Use Asset Modules for font files
+  //       generator: {
+  //         filename: 'static/fonts/[name].[hash][ext]',
+  //       },
+  //       use: {
+  //         loader: 'next-font-loader',
+  //         options: {
+  //           // Emit the font files to the public directory
+  //           publish: 'out',
+  //         },
+  //       },
+  //     });
+  //   }
+
+  //   return config;
+  // },
 };
 
 export default nextConfig;
