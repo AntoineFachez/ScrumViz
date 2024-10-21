dd55aaed62ef64b3aa6c83c6a774e3594f840536
AntoineFachez <anthony.zornig@gmx.de>
2024-10-21 14:52:15 +0200

    push

--- app/agileCoding/page.js
+++ app/agileCoding/page.js
@@ -13,38 +13,38 @@ import { Code } from '@mui/icons-material';
 
 export default function AgileCodingPage({
   uiContext,
-  startUpWidgetLayout,
+  // startUpWidgetLayout,
   // contextToolBar,
 }) {
   const containerRef = useRef(null);
 
   const [theme, colorMode, palette, styled] = useMode();
-  const { appContext, setAppContext, uiGridMapContext, setUiGridMapContext } =
-    useContext(AppContext);
+  // const { appContext, setAppContext, uiGridMapContext, setUiGridMapContext } =
+  //   useContext(AppContext);
   const { setImageUrlArr } = useContext(UIContext);
 
   const [selectedWidgetContext, setSelectedWidgetContext] =
     useState(startUpWidgetLayout);
 
   const collection = 'agileCoding';
-  const widgetProps = {
-    appContext: appContext,
-    hasWidgetMenu: true,
-    hasQuickMenu: true,
-    iconButton: <Code />,
-    collection: collection,
-    uiContext: uiContext,
-    // contextToolBar: contextToolBar,
-    widgetContext: selectedWidgetContext,
-    itemContext: '',
-    dropWidgetName: collection,
-    orderedBy: '',
-    onClick: () => {
-      setAppContext(collection);
-      setUiGridMapContext(collection);
-      return;
-    },
-  };
+  // const widgetProps = {
+  //   appContext: appContext,
+  //   hasWidgetMenu: true,
+  //   hasQuickMenu: true,
+  //   iconButton: <Code />,
+  //   collection: collection,
+  //   uiContext: uiContext,
+  //   // contextToolBar: contextToolBar,
+  //   widgetContext: selectedWidgetContext,
+  //   itemContext: '',
+  //   dropWidgetName: collection,
+  //   orderedBy: '',
+  //   onClick: () => {
+  //     setAppContext(collection);
+  //     setUiGridMapContext(collection);
+  //     return;
+  //   },
+  // };
 
   const spliceIn = () => {
     listAllBuckets(setImageUrlArr);
@@ -66,13 +66,13 @@ export default function AgileCodingPage({
         position: 'relative',
       }}
     >
-      <GridComponent styled={styled} />
+      {/* <GridComponent styled={styled} /> */}
     </Box>
   );
 
   return (
     <>
-      <WidgetIndexTemplate widgetProps={widgetProps} soloWidget={soloWidget} />
+      {/* <WidgetIndexTemplate widgetProps={widgetProps} soloWidget={soloWidget} /> */}
     </>
   );
 }
