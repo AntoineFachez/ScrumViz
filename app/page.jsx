a227fabadd7ba4bfe52e0aa487afb0da397b6a17
AntoineFachez <anthony.zornig@gmx.de>
2024-11-29 16:15:46 +0100

    push

--- app/page.jsx
+++ app/page.jsx
@@ -87,9 +87,7 @@ export default function BasePage({ session }) {
         <ThemeProvider theme={theme}>
           {user ? (
             <>
-              <>
-                <NavBar /> {uiElements()}
-              </>
+              <>{/* <NavBar /> {uiElements()} */}</>
             </>
           ) : (
             <>
